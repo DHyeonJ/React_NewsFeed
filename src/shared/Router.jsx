3d9d069b789bb685383f423f45b33cdcb0ccadd0
jinoc <choiae369@gmail.com>
2023-06-26 21:44:22 +0900

    Feat: 로그인 오류 코드별 처리

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../components/Login/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
-        <Route path="/login" />
+        <Route path="/" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
