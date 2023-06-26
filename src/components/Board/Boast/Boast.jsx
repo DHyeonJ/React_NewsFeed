a43ad71b9ed5e461d59ff0482a65aa22b8c4efc3
Persimmon <53931787+DHyeonJ@users.noreply.github.com>
2023-06-26 22:21:00 +0900

    feat : 각 페이지에 footer연결, footer Link연결,

--- src/components/Board/Boast/Boast.jsx
+++ src/components/Board/Boast/Boast.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../Main/Header/Header';
+import Footer from '../../Main/Footer/Footer';
+
 function Boast() {
   return (
     <>
       <Header />
       <div>Boast</div>
+      <Footer />
     </>
   );
 }
--- src/components/Main/Footer/Footer.jsx
+++ src/components/Main/Footer/Footer.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <FInfo>
+        © 2023 <Link to="/">Pinfo</Link> powered by &nbsp;
+        <Link target="_blank" to="https://github.com/DHyeonJ/React_NewsFeed">
+          NbCamp-MapJo
+        </Link>
+      </FInfo>
+    </>
+  );
 }
 
 export default Footer;
+
+const FInfo = styled.div`
+  text-align: center;
+`;
