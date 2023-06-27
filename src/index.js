b8abeef2f40648b3411eee43cdeb25adaab6f8e8
Sunwoo <dlatjsdn205@Naver.com>
2023-06-27 09:26:33 +0900

    Feat : 회원가입 유효성 검사

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './index.css';
-import App from './App';
-import './reset.css';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import './index.css';
+import './reset.css';
+import App from './App';
 import store from './redux/config/configStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
