718cfcab734d95214fe6d1b175d4b0f86d9c86bb
Been7 <qlsl707@hanmail.net>
2023-06-27 21:07:27 +0900

    Chore : userpage 생성

---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+import Header from '../components/Main/Header/Header';
+import Footer from '../components/Main/Footer/Footer';
+
+function UserPage() {
+  return (
+    <>
+      <Header />
+      <Layout></Layout>
+      <Footer />
+    </>
+  );
+}
+
+export default UserPage;
