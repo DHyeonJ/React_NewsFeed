4efea4638c7913e536e1eac98734fc9844535c69
Been7 <qlsl707@hanmail.net>
2023-06-28 23:04:40 +0900

    feat: 마이페이지

--- src/components/Join/Form.jsx
+++ src/components/Join/Form.jsx
@@ -1,166 +1,185 @@
-import React, { useEffect, useState } from ‘react’;
-import { styled } from ‘styled-components’;
-import LogoImgSrc from ‘../../../assets/logo.png’;
-import { Link, useNavigate } from ‘react-router-dom’;
-import { useDispatch, useSelector } from ‘react-redux’;
-import { logoutUser } from ‘../../../redux/modules/user’;
-import { signOut } from ‘firebase/auth’;
-import { auth, storage } from ‘../../../firebase’;
-import { getDownloadURL, ref } from ‘firebase/storage’;
-import time from ‘../../../assets/time.png’;
-function Header() {
-  const { user, postDatas } = useSelector(state => {
-    return state;
-  });
-  console.log(user);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [imageUrl, setImageUrl] = useState(‘’);
-  const defaultImg = ‘../../../assets/defaultImg.png’;
-  //currentUser.email이 path가 된다.
-  //path를 입력받아 해당되는 이미지를 불러오는 함수를 만든다
-  //useEffect속에서 받아온 path를 함수에 넣어 호출한다.
-  const getImageUrl = async imagePath => {
-    const imageRef = ref(storage, `profileImg/${imagePath}`);
-    const url = await getDownloadURL(imageRef);
-    return url;
-  };
-  /* useEffect(() => {
-    if (user.isLogin == ‘member’) {
-      const fetchImageUrl = async () => {
-        const imagePath = auth.currentUser.email;
-        const url = await getImageUrl(imagePath);
-        setImageUrl(url);
-      };
-      fetchImageUrl();
-    }
-  }, [user.isLogin]);*/
-  // console.log(user);
-  const logoutHandler = async () => {
-    await signOut(auth);
-    dispatch(logoutUser());
-    navigate(‘/’);
-  };
-  console.log(‘유저정보 =>‘, user, ‘게시글 정보 =>‘, postDatas);
-  return (
-    <HeaderBG>
-      <h1>
-        <Logo src={LogoImgSrc} onClick={() => navigate(‘/’)}></Logo>
-      </h1>
-      <StyledNav>
-        <Menu>
-          <li>
-            <StLink to=“/”>Home</StLink>
-          </li>
-          <li>
-            <StLink to=“/boast”>자랑게시판</StLink>
-          </li>
-          <li>
-            <StLink to=“/qna”>질문게시판</StLink>
-          </li>
-        </Menu>
-      </StyledNav>
-      <MyProfile>
-        <ProfileImg imageurl={imageUrl} defaultimg={defaultImg}></ProfileImg>
-        <Login>
-          {user.isLogin === ‘guest’ && (
-            <>
-              <LoginLink to=“/login”>Login</LoginLink>
-              <LoginLink to=“/join”>회원가입</LoginLink>
-            </>
-          )}
-          {user.isLogin === ‘member’ && (
-            <StyledLogOut onClick={logoutHandler}>로그아웃</StyledLogOut>
-          )}
-          {user.isLogin === ‘wait’ && <WaitLogin src={time} />}
-        </Login>
-      </MyProfile>
-    </HeaderBG>
-  );
-}
-export default Header;
-const HeaderBG = styled.header`
-  background-color: #12263A;
-  width: 100%;
-  height: 80px;
+import React, { useState } from 'react';
+import LogoImagSrc from '../../assets/logo_white.png';
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db, storage } from '../../firebase';
+import { getDownloadURL, ref, uploadBytes, uploadString } from 'firebase/storage';
+import { useNavigate } from 'react-router-dom';
+import { StyledInnerWrapper, StyledSocialLoginForm } from '../Login/Login';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { useEffect } from 'react';
+
+const FormContainer = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  height: 1080px;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-const Logo = styled.img`
-  cursor: pointer;
-  width: 80px;
-  height: 80px;
-  margin: 0 60px;
-  display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  border: 1px solid;
 `;
-const StyledNav = styled.nav`
+
+const StForm = styled.form`
   display: flex;
+  flex-direction: column;
   align-items: center;
+  gap: 20px;
+  width: 440px;
+  padding: 20px;
+  border: 2px solid #12263a;
+  border-radius: 12px;
+  box-shadow: rgba(18, 38, 58, 0.3) 0px 1px 3px 0px, rgba(18, 38, 58, 0.1) 0px 1px 2px 0px;
 `;
-const Menu = styled.ul`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 70px;
-  color: #fff;
+
+const StInput = styled.input`
+  width: 400px;
+  height: 40px;
+  font-size: 18px;
+  padding: 3px 20px;
+  border: 1px solid #12263a;
+  border-radius: 8px;
+  outline: none;
+  box-shadow: rgba(18, 38, 58, 0.1) 0px 1px 3px 0px, rgba(18, 38, 58, 0.06) 0px 1px 2px 0px;
+  &:focus {
+    border: 2px solid #f8db5c;
+  }
+  &::placeholder {
+    opacity: 0.4;
+  }
 `;
+
 const StLink = styled(Link)`
+  text-decoration-line: none;
+  color: black;
+`;
+
+const JoinButton = styled.button`
+  width: 400px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: #12263a;
+  cursor: pointer;
+  outline: none;
   color: #fff;
   font-size: 18px;
+  font-weight: 800;
   &:hover {
-    color: #F8DB5C;
-    font-weight: 500;
+    color: #f8db5c;
+    font-weight: 600;
+  }
+  &:focus {
+    color: #f8db5c;
   }
 `;
-const StyledLogOut = styled.p`
-  cursor: pointer;
-  font-size: 16px;
-  &:hover {
-    color: #F8DB5C;
-    font-weight: 500;
-  }
-`;
-const Login = styled.span`
+export const Logo = styled.img`
   width: 100px;
-  font-size: 20px;
+  height: 100px;
+  margin-left: 60px;
   display: flex;
-  color: #fff;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  margin-right: 15px;
-  & span {
-    margin-left: 15px;
-  }
+  margin-left: 0px;
 `;
-const LoginLink = styled(Link)`
-  margin-left: 15px;
-  font-size: 15px;
-  color: #fff;
-  &:hover {
-    color: #F8DB5C;
-    font-weight: 500;
-  }
+
+// const Sthr = styled.hr`
+// width: 100%;
+// border: 1px solid rgba(0, 0, 0, 0.1);
+// margin: 15px 0px;
+// `;
+
+const WarningMsg = styled.h3`
+  color: red;
 `;
-const WaitLogin = styled.img`
-  width: 20px;
-`;
-const MyProfile = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-`;
-const ProfileImg = styled.div`
-  cursor: pointer;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  border: 2px solid white;
-  background-image: url(${props => (props.image == ‘’ ? props.defaultimg : props.imageurl)});
-  background-size: cover;
-  background-position: center;
-`;
+
+function Form() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userPw, setUserPw] = useState('');
+  const [profileImg, setProfileImg] = useState('');
+  const [confirmPw, setConfirmPw] = useState('');
+  const [failMsg, setFailMsg] = useState('');
+  let regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  const [users, setUsers] = useState('');
+
+  const navigate = useNavigate();
+
+  const onClickJoinHandler = async e => {
+    e.preventDefault();
+
+    /// 비밀번호 확인
+    if (userPw !== confirmPw) {
+      setFailMsg('비밀번호가 일치하지 않습니다.');
+    } else if (!regex.test(userEmail)) {
+      setFailMsg('유효한 이메일을 입력해주세요');
+    } else {
+      //DB에 저장, 로그인페이지로 이동
+
+      try {
+        await createUserWithEmailAndPassword(auth, userEmail, userPw);
+        navigate('/login');
+        const newUser = {
+          userEmail,
+          userName,
+          uid: auth.currentUser.uid,
+          photoUrl: ''
+        };
+        const usersRef = collection(db, 'users');
+        addDoc(usersRef, newUser);
+      } catch (error) {
+        // 회원가입 실패시
+        if (error == 'FirebaseError: Firebase: Error (auth/email-already-in-use).') {
+          setFailMsg('이미 존재하는 이메일입니다.');
+        }
+      }
+    }
+  };
+
+  return (
+    <FormContainer>
+      <StyledInnerWrapper>
+        <Logo src={LogoImagSrc}></Logo>
+        <StForm onSubmit={e => onClickJoinHandler(e)}>
+          <StInput
+            value={userEmail}
+            onChange={e => setUserEmail(e.target.value)}
+            type="email"
+            required
+            placeholder="이메일"
+          ></StInput>
+          <StInput
+            value={userName}
+            onChange={e => setUserName(e.target.value)}
+            type="text"
+            required
+            placeholder="이름"
+          />
+          <StInput
+            type="password"
+            value={userPw}
+            minLength="8"
+            required
+            onChange={e => setUserPw(e.target.value)}
+            placeholder="비밀번호"
+          />
+          <StInput
+            type="password"
+            value={confirmPw}
+            required
+            onChange={e => setConfirmPw(e.target.value)}
+            placeholder="비밀번호 확인"
+          />
+          {failMsg && <WarningMsg>{failMsg}</WarningMsg>}
+          <JoinButton type="submit">회원가입</JoinButton>
+        </StForm>
+        <StyledSocialLoginForm>
+          <JoinButton>구글 아이디로 회원가입</JoinButton>
+          <StLink to="/login">로그인하기</StLink>
+        </StyledSocialLoginForm>
+      </StyledInnerWrapper>
+    </FormContainer>
+  );
+}
+
+export default Form;
