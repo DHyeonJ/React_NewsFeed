c7eb70d5dc56ef6b84fc32d57d309da70bac40de
Been7 <qlsl707@hanmail.net>
2023-06-28 16:24:00 +0900

    Chore : ~ing

--- src/components/Main/Header/Header.jsx
+++ src/components/Main/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from ‘react’;
 import { styled } from ‘styled-components’;
 import LogoImgSrc from ‘../../../assets/logo.png’;
 import { Link, useNavigate } from ‘react-router-dom’;
