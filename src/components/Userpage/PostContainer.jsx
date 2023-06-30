11377d9fcc8ebf0e07993df0cd2fc85e77f6eeab
Been7 <qlsl707@hanmail.net>
2023-06-30 16:21:07 +0900

    feat : 마이페이지 uid로 게시글 불러오기

--- src/components/Userpage/PostContainer.jsx
+++ src/components/Userpage/PostContainer.jsx
@@ -1,44 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { auth } from '../../firebase';
 import UserProfile from './UserProfile';
 import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 function PostContainer() {
   const loginUser = useSelector(state => state.user);
-  const { email, userName } = loginUser;
-
-  const [users, setUsers] = useState([]);
-  const myUser = users.find(user => user.userEmail === email);
+  const { uid, userName, email } = loginUser;
 
   const postDatas = useSelector(state => state.postDatas);
-  const myPost = postDatas.filter(post => post.userEmail === email);
+  const myPost = postDatas.filter(post => post.uid === uid);
 
   const commentDatas = useSelector(state => state.comments);
-  const myComment = commentDatas.filter(comment => comment.userId === email);
+  const myComment = commentDatas.filter(comment => comment.uid === uid);
 
   const navigate = useNavigate();
-
-  // firebase에 새로운 데이터 저장하기
-  useEffect(() => {
-    const fetchData = async () => {
-      // collection 이름이 users인 collection의 모든 document를 가져옵니다.
-      const q = query(collection(db, 'users'));
-      const querySnapshot = await getDocs(q);
-
-      const initialUsers = [];
-
-      querySnapshot.forEach(doc => {
-        initialUsers.push({ id: doc.id, ...doc.data() });
-      });
-      setUsers(initialUsers);
-    };
-    fetchData();
-  }, []);
 
   return (
     <div>
@@ -147,9 +123,7 @@ const MyTitleComment = styled.p`
 const ListScroll = styled.div`
   height: 138px;
   overflow: auto;
-  width: 100%; //자동으로 설정된 값과 내가 설정한 값을 쓰는 것은 다르다.
+  width: 100%;
 `;
 
 export default PostContainer;
-
-// 코드는 내가 설정한대로 돌아가는 게 제일 좋다!
